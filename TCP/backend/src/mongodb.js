0e4bff44dfee455ae58134b50ac24f95d3a3678f
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-07-13 08:57:18 -0300

    resuelto

--- TCP/backend/src/mongodb.js
+++ TCP/backend/src/mongodb.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env
 
 // 127.0.0.1
-mongoose.connect('mongodb+srv://jean:20011015kiraynana@proyecto.nn5v7j5.mongodb.net/the-clean-plate?retryWrites=true&w=majority', {
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`, {
     
     useUnifiedTopology: true,
     useNewUrlParser: true,
